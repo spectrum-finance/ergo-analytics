7b388ab6ecfbac32319718610fabc8cb712a0c99
t.gusev <gusev.t.s1@gmail.com>
2022-12-23 12:52:55 +0400

    assets processing improved

--- modules/indexer/src/main/scala/fi/spectrum/indexer/services/AssetsService.scala
+++ modules/indexer/src/main/scala/fi/spectrum/indexer/services/AssetsService.scala
@@ -12,6 +12,8 @@ import tofu.syntax.doobie.txr._
 import cats.syntax.traverse._
 import fi.spectrum.indexer.classes.syntax._
 import fi.spectrum.indexer.db.models.AssetDB._
+import fi.spectrum.indexer.models.TokenInfo
+import cats.syntax.semigroup._
 
 trait AssetsService[F[_]] {
   def process(nel: NonEmptyList[TokenId]): F[Unit]
@@ -38,11 +40,14 @@ object AssetsService {
     def process(nel: NonEmptyList[TokenId]): F[Unit] =
       for {
         exists <- cache.get
-        info   <- nel.filterNot(exists.contains).traverse(explorer.getTokenInfo).map(_.flatten)
+        toResolve = nel.distinct.toList.diff(exists)
+        info <- toResolve.filterNot(_ == TokenInfo.ErgoTokenInfo.id).traverse(explorer.getTokenInfo).map(_.flatten)
         _ <- NonEmptyList.fromList(info) match {
                case Some(value) => assetRepo.insertNoConflict(value.map(_.toDB)).trans.void
                case None        => unit[F]
              }
+        ergo = if (toResolve.contains(TokenInfo.ErgoTokenInfo.id)) List(TokenInfo.ErgoTokenInfo.id) else List.empty
+        _ <- cache.update(_ |+| info.map(_.id) |+| ergo)
       } yield ()
 
   }
