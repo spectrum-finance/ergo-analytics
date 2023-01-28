833c619a5945493ec815624dca031970b5c5d308
t.gusev <gusev.t.s1@gmail.com>
2023-01-28 13:10:10 +0400

    block id deser fixed

--- modules/streaming/src/main/scala/fi/spectrum/streaming/kafka/serde.scala
+++ modules/streaming/src/main/scala/fi/spectrum/streaming/kafka/serde.scala
@@ -2,7 +2,7 @@ package fi.spectrum.streaming.kafka
 
 import cats.effect.Sync
 import cats.syntax.applicative._
-import fi.spectrum.core.domain.TxId
+import fi.spectrum.core.domain.{BlockId, TxId}
 import fs2.kafka.{Deserializer, RecordDeserializer, RecordSerializer, Serializer}
 import io.circe.Encoder
 import io.circe.syntax._
@@ -14,6 +14,8 @@ object serde {
   object string {
 
     implicit def txIdDeserializer[F[_]: Sync]: RecordDeserializer[F, TxId] = deserializerString(TxId.apply)
+
+    implicit def blockIdDeserializer[F[_]: Sync]: RecordDeserializer[F, BlockId] = deserializerString(BlockId.apply)
 
     def deserializerString[F[_]: Sync, A](f: String => A): RecordDeserializer[F, A] =
       RecordDeserializer.lift(Deserializer.string.map(f))
