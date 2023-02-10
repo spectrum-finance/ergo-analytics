9813b6cd071ee7a7b05acae6a4904018a90728a6
t.gusev <gusev.t.s1@gmail.com>
2023-02-10 14:36:35 +0400

    mempool api tests

---
+++ modules/api/src/test/scala/fi/spectrum/api/db/Api.scala
@@ -0,0 +1,24 @@
+package fi.spectrum.api.db
+
+import cats.effect.IO
+import doobie.ConnectionIO
+import doobie.util.log.LogHandler
+import org.ergoplatform.ErgoAddressEncoder
+import tofu.doobie.log.EmbeddableLogHandler
+import tofu.logging.Logging.Make
+import tofu.logging.{Logging, Logs}
+
+trait Api {
+
+  implicit def lh: LogHandler = LogHandler.jdkLogHandler
+
+  implicit def logs: Logs[IO, IO] = Logs.sync[IO, IO]
+
+  implicit def logs2: Logs[IO, ConnectionIO] = Logs.sync[IO, ConnectionIO]
+
+  implicit def logging: Make[IO] = Logging.Make.plain[IO]
+
+  implicit def addressEncoder: ErgoAddressEncoder = ErgoAddressEncoder(ErgoAddressEncoder.MainnetNetworkPrefix)
+
+  implicit def elh: EmbeddableLogHandler[ConnectionIO] = EmbeddableLogHandler.nop
+}
