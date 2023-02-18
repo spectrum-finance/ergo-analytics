0a2fc8ae3a2b1285c697af49ebf0df57fb958aa6
t.gusev <gusev.t.s1@gmail.com>
2023-02-18 20:01:07 +0400

    v3 parsers

--- modules/api/src/test/scala/fi/spectrum/api/db/Api.scala
+++ modules/api/src/test/scala/fi/spectrum/api/db/Api.scala
@@ -3,12 +3,15 @@ package fi.spectrum.api.db
 import cats.effect.IO
 import doobie.ConnectionIO
 import doobie.util.log.LogHandler
+import fi.spectrum.core.domain.TokenId
 import org.ergoplatform.ErgoAddressEncoder
 import tofu.doobie.log.EmbeddableLogHandler
 import tofu.logging.Logging.Make
 import tofu.logging.{Logging, Logs}
 
 trait Api {
+
+  implicit val spf: TokenId = TokenId.unsafeFromString("")
 
   implicit def lh: LogHandler = LogHandler.jdkLogHandler
 
