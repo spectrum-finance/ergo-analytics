900d73d1769551b6f91867d98df9ed0adb9a359e
t.gusev <gusev.t.s1@gmail.com>
2023-02-19 18:50:19 +0400

    fix

--- modules/api/src/main/scala/fi/spectrum/api/modules/AmmStatsMath.scala
+++ modules/api/src/main/scala/fi/spectrum/api/modules/AmmStatsMath.scala
@@ -1,15 +1,21 @@
 package fi.spectrum.api.modules
 
-import cats.Monad
+import cats.{Functor, Monad}
 import cats.effect.Clock
+import derevo.derive
 import fi.spectrum.api.db.models.amm.PoolInfo
 import fi.spectrum.api.domain.{FeePercentProjection, Fees, TotalValueLocked}
+import tofu.higherKind.Mid
+import tofu.higherKind.derived.representableK
+import tofu.logging.{Logging, Logs}
 import tofu.syntax.monadic._
+import tofu.syntax.logging._
 import tofu.syntax.time.now._
 
 import scala.concurrent.duration.FiniteDuration
 import scala.math.BigDecimal.RoundingMode
 
+@derive(representableK)
 trait AmmStatsMath[F[_]] {
 
   def feePercentProjection(
@@ -22,19 +28,51 @@ trait AmmStatsMath[F[_]] {
 
 object AmmStatsMath {
 
-  implicit def instance[F[_]: Monad: Clock]: AmmStatsMath[F] =
-    (tvl: TotalValueLocked, fees: Fees, poolInfo: PoolInfo, projectionPeriod: FiniteDuration) =>
+  def make[I[_]: Functor, F[_]: Monad: Clock](implicit logs: Logs[I, F]): I[AmmStatsMath[F]] =
+    logs.forService[AmmStatsMath[F]].map { implicit __ =>
+      new Tracing[F] attach new Live[F]
+    }
+
+  final class Live[F[_]: Monad: Clock] extends AmmStatsMath[F] {
+
+    def feePercentProjection(
+      tvl: TotalValueLocked,
+      fees: Fees,
+      poolInfo: PoolInfo,
+      projectionPeriod: FiniteDuration
+    ): F[FeePercentProjection] =
       for {
         windowSizeMillis <-
           for {
             ub <- fees.window.to.fold(millis[F])(_.pure[F])
             lb = fees.window.from.getOrElse(poolInfo.firstSwapTimestamp)
           } yield ub - lb
+        _ = println(windowSizeMillis)
         periodFees =
           if (windowSizeMillis > 0) fees.value * (BigDecimal(projectionPeriod.toMillis) / windowSizeMillis)
           else BigDecimal(0)
+        _ = println(periodFees)
         periodFeesPercent =
-          if (tvl.value > 0) (periodFees * 100 / tvl.value).setScale(2, RoundingMode.HALF_UP).toDouble
-          else .0
+          if (tvl.value > 0) {
+            (periodFees * 100 / tvl.value).setScale(2, RoundingMode.HALF_UP).toDouble
+          } else .0
+        _ = println(periodFeesPercent)
       } yield FeePercentProjection(periodFeesPercent)
+  }
+
+  final class Tracing[F[_]: Monad: Logging] extends AmmStatsMath[Mid[F, *]] {
+
+    def feePercentProjection(
+      tvl: TotalValueLocked,
+      fees: Fees,
+      poolInfo: PoolInfo,
+      projectionPeriod: FiniteDuration
+    ): Mid[F, FeePercentProjection] =
+      for {
+        _ <- trace"feePercentProjection(tvl=$tvl,fees=$fees,poolInfo=$poolInfo,projectionPeriod=$projectionPeriod)"
+        r <- _
+        _ <-
+          trace"feePercentProjection(tvl=$tvl,fees=$fees,poolInfo=$poolInfo,projectionPeriod=$projectionPeriod) -> $r"
+      } yield r
+  }
 }
---
+++ modules/api/src/test/scala/fi/spectrum/api/services/AmmStatsMathSpec.scala
@@ -0,0 +1,53 @@
+package fi.spectrum.api.services
+
+import cats.effect.IO
+import cats.effect.unsafe.implicits.global
+import fi.spectrum.api.domain.{Fees, TotalValueLocked}
+import fi.spectrum.api.models.{Currency, CurrencyId, FiatUnits}
+import fi.spectrum.api.modules.AmmStatsMath
+import fi.spectrum.api.v1.endpoints.models.TimeWindow
+import org.scalatest.matchers.should
+import org.scalatest.propspec.AnyPropSpec
+import cats.syntax.option._
+import fi.spectrum.api.db.models.amm.PoolInfo
+import tofu.logging.Logs
+
+import scala.concurrent.duration.DurationInt
+
+class AmmStatsMathSpec extends AnyPropSpec with should.Matchers  {
+
+  // fees -> window=TimeWindow{from=1676730323161,to=1684619561161}
+
+  //PoolStats{
+  // id=9916d75132593c8b07fe18bd8d583bda1652eed7565cf41a4738ddd90fc992ec,
+  // lockedX=FullAsset{id=0000000000000000000000000000000000000000000000000000000000000000,
+  // amount=120752939689077,
+  // ticker=ERG,decimals=9
+  // },
+  // lockedY=FullAsset{id=03faf2cb329f2e90d6d23b58d91bbb6c046aa143261cc21f52fbe2824bfcbf04,
+  // amount=19787311,
+  // ticker=SigUSD,decimals=2
+  // },tvl=TotalValueLocked{value=41671250,units=FiatUnits{currency=Currency{id=USD,decimals=2}}},
+  //  volume=Volume{value=98539,units=FiatUnits{currency=Currency{id=USD,decimals=2}},
+  // window=TimeWindow{from=1676730323161,to=1684619561161}},
+  // fees=Fees{value=492,units=FiatUnits{currency=Currency{id=USD,decimals=2}},window=TimeWindow{from=1676730323161,to=1684619561161}},
+  // yearlyFeesPercent=0.0}
+
+  //2125798066
+  //120
+
+  property("Get ERG/USD rate") {
+    implicit val logs: Logs[IO, IO] = Logs.sync[IO, IO]
+    val tw = TimeWindow(1676730323161L.some,1684619561161L.some)
+    val math = AmmStatsMath.make[IO, IO].unsafeRunSync()
+
+    val res = math.feePercentProjection(
+      TotalValueLocked(BigDecimal(41671250),FiatUnits(Currency(CurrencyId("USD"), 2))),
+      Fees(BigDecimal(492),  FiatUnits(Currency(CurrencyId("USD"), 2)), tw),
+      PoolInfo(1630335881195L),
+      365.days
+    ).unsafeRunSync()
+
+    println(res)
+  }
+}
