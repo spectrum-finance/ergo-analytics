0b690b6d44c638cb0660cf5f5e427e339a977528
t.gusev <gusev.t.s1@gmail.com>
2023-02-28 15:55:19 +0400

    metrics improved

--- modules/core/src/main/scala/fi/spectrum/core/syntax/ProcessedOps.scala
+++ modules/core/src/main/scala/fi/spectrum/core/syntax/ProcessedOps.scala
@@ -11,11 +11,13 @@ object ProcessedOps {
     def metric: String = {
       val state = value.state.status.show.toLowerCase
       value.order match {
-        case _: Order.Deposit        => s"deposit.$state"
-        case _: Order.Redeem         => s"redeem.$state"
-        case _: Order.Swap           => s"swap.$state"
-        case _: Order.Lock           => s"lock.$state"
-        case _: Order.Compound => s"compound.$state"
+        case _: Order.Deposit.AmmDeposit => s"amm.deposit.$state"
+        case _: Order.Deposit.LmDeposit  => s"lm.deposit.$state"
+        case _: Order.Redeem.AmmRedeem   => s"amm.redeem.$state"
+        case _: Order.Redeem.LmRedeem    => s"lm.redeem.$state"
+        case _: Order.Swap               => s"amm.swap.$state"
+        case _: Order.Lock               => s"lq.lock.$state"
+        case _: Order.Compound           => s"lm.compound.$state"
       }
     }
   }
