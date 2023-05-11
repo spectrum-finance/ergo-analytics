79211769451b8cd6d016a70adba82095883762a5
t.gusev <gusev.t.s1@gmail.com>
2023-05-11 16:28:11 +0300

    mempool performance improvements

--- modules/mempool/src/main/scala/fi/spectrum/mempool/cache/RedisCache.scala
+++ modules/mempool/src/main/scala/fi/spectrum/mempool/cache/RedisCache.scala
@@ -1,43 +1,26 @@
 package fi.spectrum.mempool.cache
 
-import cats.Monad
-import cats.effect.kernel.GenConcurrent
-import cats.effect.std.Semaphore
 import dev.profunktor.redis4cats.RedisCommands
-import dev.profunktor.redis4cats.tx.TxStore
-import tofu.syntax.monadic._
-
-import scala.concurrent.duration.FiniteDuration
+import dev.profunktor.redis4cats.effects.ScriptOutputType
 
 trait RedisCache[F[_]] {
   def keys(key: String): F[List[String]]
-  def transact_(fs: List[F[Unit]]): F[Unit]
-  def del(key: String): F[Long]
-  def setEx(key: String, value: String, expiresIn: FiniteDuration): F[Unit]
   def get(key: String): F[Option[String]]
+  def eval(script: String, keys: List[String], values: List[String]): F[Unit]
 }
 
 object RedisCache {
 
-  def make[F[_]: GenConcurrent[*[_], Throwable]](implicit redis: RedisCommands[F, String, String]): F[RedisCache[F]] =
-    Semaphore[F](1).map(new Live[F](_))
+  def make[F[_]](implicit redis: RedisCommands[F, String, String]): RedisCache[F] =
+    new Live[F]
 
-  final private class Live[F[_]: Monad](semaphore: Semaphore[F])(implicit redis: RedisCommands[F, String, String])
-    extends RedisCache[F] {
+  final private class Live[F[_]](implicit redis: RedisCommands[F, String, String]) extends RedisCache[F] {
 
     def keys(key: String): F[List[String]] = redis.keys(key)
 
-    def transact_(fs: List[F[Unit]]): F[Unit] =
-      for {
-        _ <- semaphore.acquire
-        _ <- redis.transact_(fs)
-        _ <- semaphore.release
-      } yield ()
-
-    def del(key: String): F[Long] = redis.del(key)
-
-    def setEx(key: String, value: String, expiresIn: FiniteDuration): F[Unit] = redis.setEx(key, value, expiresIn)
-
     def get(key: String): F[Option[String]] = redis.get(key)
+
+    def eval(script: String, keys: List[String], values: List[String]): F[Unit] =
+      redis.eval(script, ScriptOutputType.Status, keys, values)
   }
 }
